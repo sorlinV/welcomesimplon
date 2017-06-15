c60db8d3d069b8b70b43dd91946f03b9fed37549
isidoris <valentin.sorlin@hotmail.com>
2017-06-15 09:44:15 +0200

    completed ugly

--- index.php
+++ index.php
@@ -4,18 +4,68 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Pangolin" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Sorlin Valentin</title>
 </head>
 <body>
+    <header>
+        <img src="img/default.png" id="logo" alt="logo">
+        <h1>Portfolio de SORLIN Valentin</h1>
+    </header>
+    <main>
     <?php
-        $projets = scandir(".");
+        $projets = scandir("..");
+        if (isset($_GET['proj']) && $_GET['proj'] == "false") {
+            echo "<h2>Le projet selectioné n'a pas de page d'accueil ou \n".
+            "n'est pas un projet web merci de vous reférer au github du projet.</h2>";
+        }
+        class projet {
+            public $name;
+            public $git;
+            public $img;
+        }
+        function verif_web($path) {
+            if (file_exists($path."/index.php")) {
+                return $path;
+            } elseif (file_exists($path."/index.html")) {
+                return $path;
+            } else {
+                return "?proj=false";
+            }
+            return ($path);
+        }
         foreach ($projets as $projet) {
-            if (is_dir($projet) && is_dir($projet . "/.git") && file_exists($projet . "/.git/config")) {
+            $projet = "../".$projet;
+            if (is_dir($projet) && is_dir($projet . "/.git")
+                && file_exists($projet . "/.git/config")) {
+                $proj = new projet;
+                $proj->name = $projet;
                 $config = explode ("\n", file_get_contents($projet . "/.git/config"));
-                echo '<a href="' . substr($config[6], 6) . '">' .
-                substr($config[6], 6) . '</a></br>';
+                $proj->git = substr($config[6], 6);
+                if (file_exists("img/".substr($projet, 3).".png")) {
+                    $proj->img = "img/".substr($projet, 3).".png";
+                } else {
+                    $proj->img = "img/default.png";
+                }?>
+                <article>
+                    <a href="<?php verif_web($proj->name); ?>">
+                        <h2><?php echo substr($proj->name, 3); ?></h2>
+                    </a>
+                    <a href="<?php echo verif_web($proj->name); ?>" class="img_proj">
+                        <img src="<?php echo $proj->img; ?>" alt="visuel du projet:">
+                    </a>
+                    <a href="<?php echo $proj->git; ?>"  class="img_github" >
+                        <img src="img/github.png" alt="logo github">
+                    </a>
+                </article>
+                <?php
             }
         }
     ?>
+    </main>
+    <footer>
+        <a href="simplonlyon.fr">simplonlyon.fr</a>
+    </footer>
 </body>
 </html>
